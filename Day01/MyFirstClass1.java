a0cc4a6622e9abb6607e6b005d1a6da73112df00
trina <trina@Trinadh-Pavilion>
2022-05-18 22:53:18 +0530

    First Commit

---
+++ Day01/MyFirstClass1.java
@@ -0,0 +1,10 @@
+
+public class MyFirstClass1 {
+	int data=50;//Instance Variable
+	static int m=100;//Static Variable
+	public static void main(String[] args)
+	{
+		int n=90;//Local Variable
+		System.out.println("Hello World");
+	}
+}
